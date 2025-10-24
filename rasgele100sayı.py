fa2ef26f6a150df8894ed960241ebe866d734e7d
rarda65 <r.arda.65@gmail.com>
2025-10-24 08:44:18 +0300

    Update rasgele100sayı.py

--- rasgele100sayı.py
+++ rasgele100sayı.py
@@ -1,4 +1,4 @@
-rasgelesayilist=[]
+rastgelesayilist=[]
 kucuktenbuyuge=[]
 import random
 index=0
@@ -6,32 +6,32 @@ index2=0
 mahmut=0
 sayitoplami=0
 while mahmut<100:
-  rasgelesayilist.append(random.randrange(0,200))
-  sayitoplami+=rasgelesayilist[mahmut]
+  rastgelesayilist.append(random.randrange(0,200))
+  sayitoplami+=rastgelesayilist[mahmut]
   mahmut+=1
 buyuksayi=0
 kucuksayi=200
 while index<100:
-  if buyuksayi<rasgelesayilist[index]:
-    buyuksayi=rasgelesayilist[index]
+  if buyuksayi<rastgelesayilist[index]:
+    buyuksayi=rastgelesayilist[index]
   index+=1  
 while index2<100:
-  if kucuksayi>rasgelesayilist[index2]:
-    kucuksayi=rasgelesayilist[index2]
+  if kucuksayi>rastgelesayilist[index2]:
+    kucuksayi=rastgelesayilist[index2]
   index2+=1  
-sayiortalamasi=sayitoplami/len(rasgelesayilist)
+sayiortalamasi=sayitoplami/len(rastgelesayilist)
 index3=0
 fark=0
 mahmut2=0
 while index3<100:
-  fark=sayiortalamasi-rasgelesayilist[index3]
+  fark=sayiortalamasi-rastgelesayilist[index3]
   fark**=2
   mahmut2+=fark
   index3+=1
 a=mahmut2/100
 b=a**0.5
 print("rasgele sayı listesi")
-print(rasgelesayilist)
+print(rastgelesayilist)
 print("sayı toplamı")
 print(sayitoplami)
 print("sayı ortalaması")
@@ -42,18 +42,19 @@ print("en küçük sayı")
 print(kucuksayi)
 print("standart sapma")
 print(b)
-while len(rasgelesayilist)>0:
+while len(rastgelesayilist)>0:
  kucuksayi=200
  mahmut3=0
- while mahmut3<len(rasgelesayilist):
-  if kucuksayi>rasgelesayilist[mahmut3]:
-   kucuksayi=rasgelesayilist[mahmut3]
+ while mahmut3<len(rastgelesayilist):
+  if kucuksayi>rastgelesayilist[mahmut3]:
+   kucuksayi=rastgelesayilist[mahmut3]
   mahmut3+=1
  kucuktenbuyuge.append(kucuksayi)
- rasgelesayilist.remove(kucuksayi)
+ rastgelesayilist.remove(kucuksayi)
 c=kucuktenbuyuge[49]+kucuktenbuyuge[50]
 c/=2
 print("küçükten büyüğe sıralaması")
 print(kucuktenbuyuge)
 print("medyanı")
+
 print(c)
